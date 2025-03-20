bb76afea0936d9038dc91af7a4fc860b36774e53
nana13w <andziawysocka01@gmail.com>
2025-03-20 12:24:43 +0000

    fifth commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('Hello, World!'); 
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.carousel-item img');
+    images.forEach(img => {
+        const src = img.getAttribute('src');
+        const smallSrc = src.replace(/(\.[a-z]+)$/, '-small$1');
+        if (window.innerWidth < 992) {
+            img.setAttribute('src', smallSrc);
+        }
+    });
+});
